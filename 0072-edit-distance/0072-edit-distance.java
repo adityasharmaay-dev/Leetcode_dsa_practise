06c8e265372e55fa6dfcdf9c98212ea1f08fed78
adityasharmaay-dev <adityasharmaay@gmail.com>
2026-09-25 14:06:19 +0530

    Time: 5 ms (66.32%), Space: 47.4 MB (18.02%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -1,27 +1,48 @@
 class Solution {
     public int minDistance(String word1, String word2) {
-        int[][] dp = new int[word1.length()][word2.length()];
+        int m = word1.length();
+        int n = word2.length();
+        int[][] dp = new int[m+1][n+1];
         for (int[] row : dp) {
-            Arrays.fill(row, -1);
+            Arrays.fill(row, 0);
         }
-        return helper(word1, word2, word1.length()-1, word2.length()-1, dp);
+        for(int i=0; i<=m; i++){
+            dp[i][0] = i;
+        }
+        for(int j=0; j<=n; j++){
+            dp[0][j] = j;
+        }
+        for(int i=1; i<=m; i++){
+            for(int j=1; j<=n; j++){
+                if(word1.charAt(i-1) == word2.charAt(j-1)){
+                    dp[i][j] = dp[i-1][j-1];
+                }
+                else{
+                    int insert = 1 + dp[i][j-1];
+                    int del = 1 + dp[i-1][j];
+                    int replace = 1 + dp[i-1][j-1];
+                    dp[i][j] = Math.min(Math.min(insert,del), replace);
+                }
+            }
+        }
+        return dp[m][n];
     }
-    public int helper(String s1, String s2, int i, int j, int[][] dp){
-        if(i<0){
-            return j+1;
-        }
-        if(j<0){
-            return i+1;
-        }
-        if(dp[i][j] != -1) return dp[i][j];
+    // public int helper(String s1, String s2, int i, int j, int[][] dp){
+    //     if(i<0){
+    //         return j+1;
+    //     }
+    //     if(j<0){
+    //         return i+1;
+    //     }
+    //     if(dp[i][j] != -1) return dp[i][j];
 
-        if(s1.charAt(i) == s2.charAt(j)){
-            return dp[i][j] = helper(s1, s2, i-1, j-1, dp);
-        }
-        int insert = 1 + helper(s1,s2,i,j-1, dp);
-        int del = 1 + helper(s1,s2,i-1,j, dp);
-        int replace = 1 + helper(s1,s2,i-1,j-1, dp);
+    //     if(s1.charAt(i) == s2.charAt(j)){
+    //         return dp[i][j] = helper(s1, s2, i-1, j-1, dp);
+    //     }
+    //     int insert = 1 + helper(s1,s2,i,j-1, dp);
+    //     int del = 1 + helper(s1,s2,i-1,j, dp);
+    //     int replace = 1 + helper(s1,s2,i-1,j-1, dp);
 
-        return dp[i][j] = Math.min(Math.min(insert,del), replace);
-    }
+    //     return dp[i][j] = Math.min(Math.min(insert,del), replace);
+    // }
 }
